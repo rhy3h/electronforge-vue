c46e937d5e0eed70a37120470732b39e3d4ac24d
rhy3h <rhy3htw@gmail.com>
2023-03-19 23:34:25 +0800

    feat: 移除建置 Deb, Rpm 平台, Zip 建置 win32

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,6 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
 import { mainConfig } from "./webpack/webpack.main.config";
@@ -11,12 +9,7 @@ import { rendererConfig } from "./webpack/webpack.renderer.config";
 const config: ForgeConfig = {
   packagerConfig: {},
   rebuildConfig: {},
-  makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin"]),
-    new MakerRpm({}),
-    new MakerDeb({}),
-  ],
+  makers: [new MakerSquirrel({}), new MakerZIP({}, ["win32"])],
   plugins: [
     new WebpackPlugin({
       mainConfig,
