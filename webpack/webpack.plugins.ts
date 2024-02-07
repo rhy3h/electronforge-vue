5e5b8fb89cb2fa7367e5149cbb4b48e1ed005d0e
rhy3h <rhy3htw@gmail.com>
2024-02-07 15:15:59 +0800

    refactor: Adjust code under ESLint

--- webpack/webpack.plugins.ts
+++ webpack/webpack.plugins.ts
@@ -1,8 +1,5 @@
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { VueLoaderPlugin } from 'vue-loader';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
